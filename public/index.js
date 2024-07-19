d3b28cc9b2aec330de18040fb157f6b8380be9a0
NuranR <ranawakanuran@gmail.com>
2024-07-19 11:35:27 +0530

    Change bot icon

--- public/index.js
+++ public/index.js
@@ -91,7 +91,7 @@ function initBotConversation() {
         domain: domain
     });
     const styleOptions = {
-        botAvatarImage: 'https://docs.microsoft.com/en-us/azure/bot-service/v4sdk/media/logo_bot.svg?view=azure-bot-service-4.0',
+        botAvatarImage: 'https://www.mediwave.io/wp-content/uploads/2023/02/cropped-mediwave-logo-icon.png',
         // botAvatarInitials: '',
         // userAvatarImage: '',
         hideSendBox: false, /* set to true to hide the send box from the view */
